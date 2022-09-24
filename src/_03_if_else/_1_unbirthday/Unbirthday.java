46d95682a5fa9f016f1989e2d543a74637931351
jeremycui2008 <jeremycuid@gmail.com>
2022-09-24 16:31:59 -0700

    Unbirthday

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,26 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	
+	
+  public static void main(String[] args) {
+	  String input = JOptionPane.showInputDialog("When's your birthday");	
+if(input.equals("September 24")) {
+	JOptionPane.showInputDialog(null,"Happy Birthday!");
+}
+	else {
+		JOptionPane.showMessageDialog(null,"I wish you a very merry UNbirthday");
+	}
+		
+
+
+  
+  
+  
+  
+  }
+	
+}
+
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -9,7 +9,9 @@ public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
 
-		// 2. Ask the user to enter a name. Store their answer in a variable.
+	String input = JOptionPane.showInputDialog("Say someones name.");
+	
+	// 2. Ask the user to enter a name. Store their answer in a variable.
 
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
 
